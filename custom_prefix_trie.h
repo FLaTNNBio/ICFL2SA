43070c0ef234256234bf3a52d8fe405e40359319
Antonio <antonio.allocca12345@live.it>
2024-07-22 18:43:29 +0200

    ultime modifiche

--- custom_prefix_trie.h
+++ custom_prefix_trie.h
@@ -12,6 +12,6 @@ typedef struct custom_prefix_trie{
 
 custom_prefix_trie init_custom_prefix_trie();
 void add_in_custom_prefix_trie(custom_prefix_trie* root,const char* S,const char* suffix,int current_suffix_len,int suffix_len,int suffix_index,vector<int>& icfl_list,vector<int>& custom_icfl_list,int lenght_of_word,vector<int>& is_custom_vec,vector<int>& factor_list);
-void compute_i_phase_alberello_custom_prefix_trie(const char*S,int lenght_of_word,vector<int>& icfl_list,int icfl_size,vector<int>& custom_icfl_list,int custom_icfl_size,suffix_tree_node* root,int i,vector<int>& is_custom_vec,vector<int>& factor_list);
-void add_node_in_suffix_tree_alberello_custom_prefix_trie(const char* S,vector<int> icfl_list,int icfl_size,custom_prefix_trie * root,int i,int j);
+void compute_i_phase_alberello_custom_prefix_trie(const char*S,int lenght_of_word,vector<int>& icfl_list,int icfl_size,vector<int>& custom_icfl_list,int custom_icfl_size,custom_prefix_trie* root,int i,vector<int>& is_custom_vec,vector<int>& factor_list);
+void add_in_custom_prefix_trie(custom_prefix_trie* root,const char* S,const char* suffix,int current_suffix_len,int suffix_len,int suffix_index,vector<int>& icfl_list,vector<int>& custom_icfl_list,int lenght_of_word,vector<int>& is_custom_vec,vector<int>& factor_list);
 custom_prefix_trie creazione_albero_custom_prefix_trie(vector<int>& icfl_list,vector<int>& custom_icfl_list,const char* S,int lenght_of_word,int max_size,int custom_max_size,int n_threads);
